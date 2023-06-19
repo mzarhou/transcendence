8491a2327f8017ca5783a263195a7e29bf0c0d9a
mzarhou <mzarhou.cc@gmail.com>
2023-06-19 03:52:10 +0100

    style: sort tailwind css classes

--- apps/frontend/app/profile/layout.tsx
+++ apps/frontend/app/profile/layout.tsx
@@ -2,13 +2,14 @@ import { BarChart3, Settings, SlidersHorizontal } from "lucide-react";
 import { ReactNode } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import Auth from "@/components/Auth";
+import { cn } from "@/lib/utils";
 
 type ProfileLayoutProps = {
   children: ReactNode;
 };
 export default function ProfileLayout({ children }: ProfileLayoutProps) {
   return (
-    <div className="flex mt-4 md:space-x-12">
+    <div className="mt-4 flex md:space-x-12">
       <Auth>
         <ProfileSideBar />
         {children}
@@ -19,7 +20,7 @@ export default function ProfileLayout({ children }: ProfileLayoutProps) {
 
 function ProfileSideBar() {
   return (
-    <div className="w-full max-w-[200px] sticky top-28">
+    <div className="sticky top-28 w-full max-w-[200px]">
       <ul>
         <li>
           <ProfileSideBarItem text="general" selected>
@@ -49,9 +50,12 @@ export function ProfileSideBarItem({
 }: ProfileSideBarItemProps) {
   return (
     <div
-      className={`space-x-4 flex items-center mb-2 py-2 rounded-md hover:bg-gray-100 px-3 cursor-pointer ${
-        !selected && "text-gray-300"
-      }`}
+      className={cn(
+        "mb-2 flex cursor-pointer items-center space-x-4 rounded-md px-3 py-2 hover:bg-gray-100 hover:text-black",
+        {
+          "text-gray-300": !selected,
+        }
+      )}
     >
       <Slot className="icon-container icon-md">{children}</Slot>
       <span className="text-base">{text}</span>
