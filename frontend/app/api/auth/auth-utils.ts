89fa7fc02b249a285b1e5da919fc63013592ca44
mzarhou <mzarhou.cc@gmail.com>
2023-07-25 03:45:32 +0100

    fix(ui): cookies domain

--- frontend/app/api/auth/auth-utils.ts
+++ frontend/app/api/auth/auth-utils.ts
@@ -1,5 +1,14 @@
+import { env } from "@/env/server.mjs";
 import { TokensResponse } from "@/schema/auth-schema";
 import { NextRequest, NextResponse } from "next/server";
+
+function getCookieDomain() {
+  const domain = env.APP_URL.split("https://")[1].split(".");
+  if (domain.length > 2) {
+    return domain.slice(1).join(".");
+  }
+  return domain.join(".");
+}
 
 export function setCookies(
   response: NextResponse<unknown>,
@@ -7,11 +16,12 @@ export function setCookies(
 ) {
   type CookieConfig = Parameters<typeof response.cookies.set>[2];
 
-  // TODO: secure the cookie
   const cookieConfig: CookieConfig = {
     httpOnly: true,
     sameSite: "lax",
     secure: process.env.NODE_ENV === "production" ? true : false,
+    domain:
+      process.env.NODE_ENV === "production" ? getCookieDomain() : undefined,
   };
   response.cookies.set("accessToken", tokens.accessToken, cookieConfig);
   response.cookies.set("refreshToken", tokens.refreshToken, cookieConfig);
