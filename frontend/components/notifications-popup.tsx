1d74f8559f52b7996aa96b4d9f357f28bd6d8b1b
mzarhou <mzarhou.cc@gmail.com>
2023-09-04 20:14:06 +0100

    fix(ui): group notifications

--- frontend/components/notifications-popup.tsx
+++ frontend/components/notifications-popup.tsx
@@ -19,6 +19,15 @@ import {
 } from "@transcendence/common";
 import { LoaderButton } from "./ui/loader-button";
 import { NoticationsBadge } from "./ui/notifications-badge";
+import { GROUP_DELETED_NOTIFICATION } from "@transcendence/common";
+import { ADD_ADMIN_NOTIFICATION } from "@transcendence/common";
+import { REMOVE_ADMIN_NOTIFICATION } from "@transcendence/common";
+import { GROUP_BANNED_NOTIFICATION } from "@transcendence/common";
+import { GROUP_UNBANNED_NOTIFICATION } from "@transcendence/common";
+import { LEAVE_GROUP_NOTIFICATION } from "@transcendence/common";
+import { JOIN_GROUP_NOTIFICATION } from "@transcendence/common";
+import { GROUP_KICKED_NOTIFICATION } from "@transcendence/common";
+import { GROUP_NOTIFICATION_PAYLOAD } from "@transcendence/common";
 
 export default function NotificationsPopup() {
   const [open, setOpen] = useState(false);
@@ -42,7 +51,7 @@ export default function NotificationsPopup() {
 
   const unreadNotificationsCount = useMemo(
     () => notifications.filter((n) => !n.isRead).length,
-    [notifications]
+    [notifications],
   );
 
   return (
@@ -128,5 +137,26 @@ function NotificationItem({
       </div>
     );
   }
-  return <div>{nt.data}</div>;
+  if (
+    [
+      GROUP_DELETED_NOTIFICATION,
+      ADD_ADMIN_NOTIFICATION,
+      REMOVE_ADMIN_NOTIFICATION,
+      GROUP_BANNED_NOTIFICATION,
+      GROUP_UNBANNED_NOTIFICATION,
+      GROUP_KICKED_NOTIFICATION,
+      JOIN_GROUP_NOTIFICATION,
+      LEAVE_GROUP_NOTIFICATION,
+    ].includes(nt.event)
+  ) {
+    const data = nt.data as GROUP_NOTIFICATION_PAYLOAD;
+    return <data>{data.message}</data>;
+  }
+
+  if (typeof nt.data === "object") {
+    return <div>{JSON.stringify(nt.data)}</div>;
+  } else if (typeof nt.data === "string") {
+    return <div>{nt.data}</div>;
+  }
+  return <div>Unknow notification</div>;
 }
