4b23c558ea7ac392523192f8d0293589cdc9996c
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-10-11 05:10:27 +0100

    fix: asynchronous update issues

--- apps/api/src/game/rank/rank.service.ts
+++ apps/api/src/game/rank/rank.service.ts
@@ -8,30 +8,30 @@ export class RankService {
   constructor(
     private prisma: PrismaService,
     private matches: MatchesService,
-  ) {}
+  ) { }
 
   async getAllRank() {
     return this.prisma.user.findMany();
   }
 
-  async getNumOfMatchesPlayed(id: number): Promise<any>{
-    const user = await this.prisma.user.findUnique({
+  async getNumOfMatchesPlayed(id: number): Promise<any> {
+    const player = await this.prisma.user.findUnique({
       where: { id },
     });
-    return (user?.numOfGames);
+    return (player?.numOfGames);
   }
 
-  async getEloScore(id: number): Promise<any>{
-    const user = await this.prisma.user.findUnique({
+  async getEloScore(id: number): Promise<any> {
+    const player = await this.prisma.user.findUnique({
       where: { id },
     });
-    return (user?.eloRating);
+    return (player?.eloRating);
   }
 
-  async getNumOfWins(id: number): Promise<any>{
-    const user = await this.prisma.user.findUnique({
-      where: {id},
-      include:{
+  async getNumOfWins(id: number): Promise<any> {
+    const player = await this.prisma.user.findUnique({
+      where: { id },
+      include: {
         matches3: {
           where: {
             winnerId: id,
@@ -39,28 +39,28 @@ export class RankService {
         }
       }
     });
-    return (user?.matches3.length);
+    return (player?.matches3.length);
   }
 
-  async getProvRank() {
+  async getProvElo() {
     return this.prisma.user.findMany({
       where: {
         rankBoard: 'Provisional',
       },
       orderBy: {
-        rank: 'asc',
-      },
+        eloRating: 'desc',
+      }
     });
   }
 
-  async getEstaRank() {
+  async getEstaElo() {
     return this.prisma.user.findMany({
       where: {
         rankBoard: 'Established',
       },
       orderBy: {
-        rank: 'asc',
-      },
+        eloRating: 'desc',
+      }
     });
   }
 
@@ -69,103 +69,93 @@ export class RankService {
       where: { id: userId },
     });
 
-    if (!player) {
+    if (!player)
       return;
-    }
-
     await this.prisma.user.update({
       where: { id: userId },
       data: {
         numOfGames: player.numOfGames + 1,
-      },
-    });
+      }
+    })
   }
 
   async newElo(newElo: number, userId: number) {
-    const player = await this.prisma.user.findUnique({
-      where: { id: userId },
-    });
-    const uptPlayer = await this.prisma.user.update({
+    return this.prisma.user.update({
       where: { id: userId },
       data: {
         eloRating: newElo,
-      },
-    });
+      }
+    })
   }
 
   async updateRank(): Promise<void> {
-    const players = await this.prisma.user.findMany({
-      orderBy: {
-        eloRating: 'desc',
-      },
-    });
+    const estaPlayers = await this.getEstaElo();
+    const provPlayers = await this.getProvElo();
 
     let newRank = 1;
 
-    for (const player of players) {
-      await this.prisma.user.update({
-        where: { id: player.id },
-        data: { rank: newRank },
-      });
-      newRank++;
+    if (estaPlayers.length !== 0) {
+      for (const player of estaPlayers) {
+        await this.prisma.user.update({
+          where: { id: player.id },
+          data: { rank: newRank },
+        });
+        newRank++;
+      }
+    }
+
+    if (provPlayers.length !== 0) {
+      for (const player of provPlayers) {
+        await this.prisma.user.update({
+          where: { id: player.id },
+          data: { rank: newRank },
+        });
+        newRank++;
+      }
     }
   }
 
-  async updateRankBoard(): Promise<void> {
-    const players = await this.prisma.user.findMany({
+  async updateRankBoard() {
+    return this.prisma.user.updateMany({
       where: {
         rankBoard: 'Provisional',
         numOfGames: {
           gt: 20,
-        },
+        }
       },
-    });
-
-    for (const player of players) {
-      await this.prisma.user.update({
-        where: { id: player.id },
-        data: {
-          rankBoard: 'Established',
-        },
-      });
-    }
+      data: {
+        rankBoard: 'Established'
+      },
+    })
   }
 
   async updateDivision(): Promise<void> {
     const players = await this.prisma.user.findMany();
     for (const player of players) {
-      if (player.rankBoard == 'Established') {
-        if (player.eloRating < 800 && player.division != 'Nooby') {
+      if (player.rankBoard == "Established") {
+        if (player.eloRating < 800 && player.division != "Nooby") {
           await this.prisma.user.update({
             where: { id: player.id },
-            data: { division: 'Nooby' },
-          });
+            data: { division: "Nooby" },
+          })
         }
-        if (
-          player.eloRating >= 800 &&
-          player.eloRating < 1250 &&
-          player.division != 'Bronze'
-        ) {
+        if ((player.eloRating >= 800 && player.eloRating < 1250) && player.division != "Bronze") {
           await this.prisma.user.update({
             where: { id: player.id },
-            data: { division: 'Bronze' },
-          });
+            data: { division: "Bronze" },
+          })
         }
-        if (
-          player.eloRating >= 1250 &&
-          player.eloRating < 1800 &&
-          player.division != 'Gold'
-        ) {
+        if ((player.eloRating >= 1250 && player.eloRating < 1800) && player.division != "Gold") {
           await this.prisma.user.update({
             where: { id: player.id },
-            data: { division: 'Gold' },
-          });
+            data: { division: "Gold" },
+          })
         }
-        if (player.eloRating >= 1800 && player.division != 'Legend') {
+        if ((player.eloRating >= 1800) && player.division != "Legend") {
           await this.prisma.user.update({
             where: { id: player.id },
-            data: { division: 'Legend' },
-          });
+            data: { division: "Legend" },
+          })
         }
       }
     }
@@ -173,109 +163,80 @@ export class RankService {
 
   async updateElo(matchId: number) {
     const match = await this.matches.findOneById(matchId);
-    const players = await this.prisma.user.findMany();
-    const home = players[match.homeId];
-    const adversary = players[match.adversaryId];
+    const home = await this.prisma.user.findUnique({
+      where: {
+        id: match.homeId,
+      }
+    })
 
-    let s1 = 0,
-      s2 = 0,
-      newR1 = 0,
-      newR2 = 0;
+    const adversary = await this.prisma.user.findUnique({
+      where: {
+        id: match.adversaryId,
+      }
+    })
+    if (!home || !adversary)
+      return ;
+    let s1 = 0, s2 = 0, newR1 = 0, newR2 = 0;
 
-    this.incrementNumOfGames(home.id);
-    this.incrementNumOfGames(adversary.id);
+    await this.incrementNumOfGames(match.homeId);
+    await this.incrementNumOfGames(match.adversaryId);
 
-    if (
-      home.rankBoard == 'Provisional' &&
-      adversary.rankBoard == 'Provisional'
-    ) {
+    if (home.rankBoard == "Provisional" && adversary.rankBoard == "Provisional") {
       if (home.id == match.winnerId) {
         s1 = 1;
         s2 = -1;
-      } else if (adversary.id == match.winnerId) {
+      }
+      else if (adversary.id == match.winnerId) {
         s1 = -1;
         s2 = 1;
       }
-      newR1 =
-        (home.eloRating * home.numOfGames +
-          (home.eloRating + adversary.eloRating) / 2 +
-          100 * s1) /
-        (home.numOfGames + 1);
-      newR2 =
-        (home.eloRating * home.numOfGames +
-          (home.eloRating + adversary.eloRating) / 2 +
-          100 * s2) /
-        (home.numOfGames + 1);
-    } else if (
-      home.rankBoard == 'Provisional' &&
-      adversary.rankBoard == 'Established'
-    ) {
-      if (home.id == match.winnerId) {
-        s1 = 1;
-        s2 = 0;
-      } else if (adversary.id == match.winnerId) {
-        s1 = -1;
-        s2 = 1;
-      }
-      newR1 =
-        (home.eloRating * home.numOfGames + adversary.eloRating + 200 * s1) /
-        (home.numOfGames + 1);
-      newR2 =
-        home.eloRating +
-        32 *
-          (adversary.numOfGames / 20) *
-          (s2 -
-            1 /
-              (1 + Math.pow(10, (adversary.eloRating - home.eloRating) / 400)));
-    } else if (
-      home.rankBoard == 'Established' &&
-      adversary.rankBoard == 'Provisional'
-    ) {
-      if (home.id == match.winnerId) {
-        s1 = 1;
-        s2 = -1;
-      } else if (adversary.id == match.winnerId) {
-        s1 = 0;
-        s2 = 1;
-      }
-      newR1 =
-        home.eloRating +
-        32 *
-          (adversary.numOfGames / 20) *
-          (s2 -
-            1 /
-              (1 + Math.pow(10, (adversary.eloRating - home.eloRating) / 400)));
-      newR2 =
-        (home.eloRating * home.numOfGames + adversary.eloRating + 200 * s1) /
-        (home.numOfGames + 1);
-    } else if (
-      home.rankBoard == 'Established' &&
-      adversary.rankBoard == 'Established'
-    ) {
-      if (home.id == match.winnerId) {
-        s1 = 1;
-        s2 = 0;
-      } else if (adversary.id == match.winnerId) {
-        s1 = 0;
-        s2 = 1;
-      }
-      newR1 =
-        home.eloRating +
-        32 *
-          (s1 -
-            1 /
-              (1 + Math.pow(10, (adversary.eloRating - home.eloRating) / 400)));
-      newR2 =
-        home.eloRating +
-        32 *
-          (s2 -
-            1 /
-              (1 + Math.pow(10, (adversary.eloRating - home.eloRating) / 400)));
+      newR1 = ((home.eloRating * home.numOfGames + (home.eloRating + adversary.eloRating) / 2) + 100 * s1) / (home.numOfGames + 1)
+      newR2 = ((home.eloRating * home.numOfGames + (home.eloRating + adversary.eloRating) / 2) + 100 * s2) / (home.numOfGames + 1)
     }
-    this.newElo(newR1, home.id);
-    this.newElo(newR2, adversary.id);
-    this.updateRank();
-    this.updateRankBoard();
-    this.updateDivision();
+
+    else if (home.rankBoard == "Provisional" && adversary.rankBoard == "Established") {
+      if (home.id == match.winnerId) {
+        s1 = 1;
+        s2 = 0;
+      }
+      else if (adversary.id == match.winnerId) {
+        s1 = -1;
+        s2 = 1;
+      }
+      newR1 = (home.eloRating * home.numOfGames + adversary.eloRating + 200 * s1) / (home.numOfGames + 1)
+      newR2 = home.eloRating + (32 * (adversary.numOfGames / 20)) * (s2 - 1 / (1 + Math.pow(10, ((adversary.eloRating - home.eloRating) / 400))))
+    }
+
+    else if (home.rankBoard == "Established" && adversary.rankBoard == "Provisional") {
+      if (home.id == match.winnerId) {
+        s1 = 1;
+        s2 = -1;
+      }
+      else if (adversary.id == match.winnerId) {
+        s1 = 0;
+        s2 = 1;
+      }
+      newR1 = home.eloRating + (32 * (adversary.numOfGames / 20)) * (s2 - 1 / (1 + Math.pow(10, ((adversary.eloRating - home.eloRating) / 400))))
+      newR2 = (home.eloRating * home.numOfGames + adversary.eloRating + 200 * s1) / (home.numOfGames + 1)
+    }
+
+    else if (home.rankBoard == "Established" && adversary.rankBoard == "Established") {
+      if (home.id == match.winnerId) {
+        s1 = 1;
+        s2 = 0;
+      }
+      else if (adversary.id == match.winnerId) {
+        s1 = 0;
+        s2 = 1;
+      }
+      newR1 = home.eloRating + 32 * (s1 - 1 / (1 + Math.pow(10, ((adversary.eloRating - home.eloRating) / 400))))
+      newR2 = home.eloRating + 32 * (s2 - 1 / (1 + Math.pow(10, ((adversary.eloRating - home.eloRating) / 400))))
+    }
+
+    await this.newElo(newR1, home.id)
+    await this.newElo(newR2, adversary.id)
+    await this.updateRankBoard()
+    await this.updateDivision()
+    await this.updateRank()
   }
 }
