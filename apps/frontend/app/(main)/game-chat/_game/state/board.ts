a8744e10630fb1ffd4ce9cfdd42f4c5488f0ca7b
Abdelghani Abouelmahassine <abdel.abouelmahassine@gmail.com>
2023-10-18 15:32:26 +0100

    change name of enum Status to COURT

--- apps/frontend/app/(main)/game-chat/_game/state/board.ts
+++ apps/frontend/app/(main)/game-chat/_game/state/board.ts
@@ -1,7 +1,7 @@
 import { BoardType } from "@transcendence/db";
 import { create } from "zustand";
 
-export enum STATUS {
+export enum COURT {
   BBLACK = "/board_black.png",
   BBLUE = "/board_blue.png",
   BGREEN = "/board_green.png",
@@ -15,7 +15,7 @@ interface BoardState extends BoardType {
 const boardState: BoardType = {
   posi: [0, 0, 0],
   size: [600, 800],
-  txtu: STATUS.BORANGE,
+  txtu: COURT.BORANGE,
 };
 
 export const useBoardState = create<BoardState>((set) => ({
