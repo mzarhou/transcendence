ec577337b8804825fbed6e6e63db47f6d91f258b
mzarhou <mzarhou.cc@gmail.com>
2023-10-19 21:51:46 +0100

    fix(frontend): board texture not found

--- apps/frontend/app/(main)/game-chat/_game/entity/entity.tsx
+++ apps/frontend/app/(main)/game-chat/_game/entity/entity.tsx
@@ -3,5 +3,5 @@ import { BoardType } from "@transcendence/db";
 export let boardEntity: BoardType = {
   posi: [0, 0, 0],
   size: [600, 800],
-  txtu: "background.png",
+  txtu: "/background.png",
 };
